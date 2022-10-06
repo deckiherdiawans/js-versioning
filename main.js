6845844ab8e9ead2d4ca786894b3a9f87c396562
Decki Herdiawan Soepandi <d.herdiawan.s@gmail.com>
2022-10-06 10:50:46 +0700

    Last Modified date format update.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const child_process = require("child_process"), fs = require("fs")
 
-let fileInfo = {},
+let file_info = {},
     childProductVersion = child_process.spawnSync(
         "powershell.exe",
         ["(Get-Item -path D:\\revota\\travics_erigo\\dist_v47\\source\\Source-ErigoDist_v47\\api\\tito_sap\\--tito_sap.exe).VersionInfo.ProductVersion"],
@@ -18,13 +18,15 @@ let fileInfo = {},
     ),
     childLastModified = child_process.spawnSync(
         "powershell.exe",
-        ["(Get-ChildItem -path D:\\revota\\travics_erigo\\dist_v47\\source\\Source-ErigoDist_v47\\api\\tito_sap\\--tito_sap.exe).LastWriteTime"],
+        ["Get-ChildItem -path D:\\revota\\travics_erigo\\dist_v47\\source\\Source-ErigoDist_v47\\api\\tito_sap\\--tito_sap.exe | Select-Object @{n='LastWriteTime';e={'{0:yyyy-MM-dd HH:mm:ss}' -f $_.LastWriteTime}}"],
         { encoding: "utf8" }
     )
 
-fileInfo.product_version = childProductVersion.stdout.toString().replace("\r\n", "")
-fileInfo.file_version = childFileVersion.stdout.toString().replace("\r\n", "")
-fileInfo.file_name = childFileName.stdout.toString().replace("\r\n", "").replace(/\\/g, "/")
-fileInfo.last_modified = new Date(childLastModified.stdout.replace(/\r\n/g, "")).toLocaleString("sv-SE")
+file_info.product_version = childProductVersion.stdout.trim()
+file_info.file_version = childFileVersion.stdout.trim()
+file_info.file_name = childFileName.stdout.trim().replace(/\\/g, "/")
+file_info.last_modified = childLastModified.stdout.trim().replace("LastWriteTime      \r\n-------------      \r\n", "")
 
-fs.writeFileSync("./file_info.json", JSON.stringify(fileInfo))
+console.log(file_info)
+
+fs.writeFileSync("./file_info.json", JSON.stringify(file_info))
